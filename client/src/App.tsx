c925828427036826af49d91ecf0a03ecf36d9b1c
Aaron Olsen <aolsen@uwaterloo.ca>
2024-05-24 19:50:58 -0400

    Scaffold initial client and server apps

---
+++ client/src/App.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>WaTerFood</h1>
+      <p className="read-the-docs">
+        Choose your restaurant
+      </p>
+    </>
+  )
+}
+
+export default App
