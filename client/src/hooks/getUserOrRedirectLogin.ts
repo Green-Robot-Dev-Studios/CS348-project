7fdf2546a12e0a657f182fdb025f85e12385f5db
Aaron Olsen <aolsen@uwaterloo.ca>
2024-07-06 21:42:41 -0400

    Use navigate for type-safe routes

--- client/src/hooks/getUserOrRedirectLogin.ts
+++ client/src/hooks/getUserOrRedirectLogin.ts
@@ -1,11 +1,11 @@
-import { useRouter } from "@tanstack/react-router";
+import { useNavigate } from "@tanstack/react-router";
 import { useFeathers } from "figbird";
 import { useEffect, useState } from "react";
 import { User } from "waterfood";
 
 const getUserOrRedirectLogin = () => {
   const feathers = useFeathers();
-  const router = useRouter();
+  const navigate = useNavigate();
 
   const [user, setUser] = useState<User | null>(null);
 
@@ -13,7 +13,7 @@ const getUserOrRedirectLogin = () => {
     feathers
       .reAuthenticate()
       .then((auth) => setUser(auth.user))
-      .catch(() => router.history.push("/login"));
+      .catch(() => navigate({ to: "/login" }));
   }, [feathers]);
 
   return user;
