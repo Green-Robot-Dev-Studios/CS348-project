bf35f8102da82786dda435c795f5b93c5661ea7b
Aaron Olsen <aolsen@uwaterloo.ca>
2024-07-09 23:51:47 -0400

    Use checkboxes instead of toggles so users know what to do

--- client/src/routes/preferences.$roomId.lazy.tsx
+++ client/src/routes/preferences.$roomId.lazy.tsx
@@ -1,7 +1,7 @@
 import { Content } from "@/components/content";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
-import { Toggle } from "@/components/ui/toggle";
+import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card";
+import { Checkbox } from "@/components/ui/checkbox";
 import { createLazyFileRoute, useNavigate } from "@tanstack/react-router";
 
 export const Route = createLazyFileRoute("/preferences/$roomId")({
@@ -18,7 +18,6 @@ export function Preferences() {
 
   const handleChange = (preference: string) => {
     selectedPrefs.set(preference, !selectedPrefs.get(preference));
-    // console.log(selectedPrefs);
   };
 
   const handleStart = () => {
@@ -26,27 +25,28 @@ export function Preferences() {
   };
 
   return (
-    <Content>
-      <div className="mt-10 h-96">
-        <div className="mx-10">
-          <Card>
-            <CardHeader>Select your preferences!</CardHeader>
-            <CardContent>
-              {preferences.map((preference: string) => (
-                <div key={preference}>
-                  <Toggle className="mx-1 my-1 text-lg" value={preference} onClick={() => handleChange(preference)}>
-                    {preference.charAt(0).toUpperCase() + preference.slice(1)}
-                  </Toggle>
-                  <br />
-                </div>
-              ))}
-            </CardContent>
-            <Button onClick={handleStart} className="m-6 mt-0">
-              Start swiping!
-            </Button>
-          </Card>
-        </div>
-      </div>
+    <Content className="flex flex-row justify-center">
+      <Card className="flex w-full max-w-lg flex-col">
+        <CardHeader>Select your preferences!</CardHeader>
+        <CardContent className="flex-grow">
+          {preferences.map((preference) => (
+            <div key={preference} className="ml-2 flex items-center space-x-2">
+              <Checkbox id={preference} onChange={() => handleChange(preference)} />
+              <label
+                htmlFor={preference}
+                className="m-1 text-lg peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+              >
+                {preference.charAt(0).toUpperCase() + preference.slice(1)}
+              </label>
+            </div>
+          ))}
+        </CardContent>
+        <CardFooter>
+          <Button onClick={handleStart} className="w-full">
+            Start swiping!
+          </Button>
+        </CardFooter>
+      </Card>
     </Content>
   );
 }
